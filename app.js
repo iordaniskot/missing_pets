1bb7feb820953a5de63cce4c1d4d97ebb3c304cf
iordaniskot <iordaniskotiadis@gmail.com>
2025-05-24 00:39:33 +0300

    Implement server setup with middleware, error handling, and MongoDB connection; update JWT secret handling and add product requirements document.

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 const mongoose = require("mongoose");
 const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
 
-const dotenv = require("dotenv");
+const app = express();
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@missingpet.mrdfzgj.mongodb.net/${process.env.DEFAULT_DATABASE}?authSource=admin&retryWrites=true&w=majority`;
+
+app.use(bodyParser.urlencoded({ extended: true })); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use("/attachments", express.static(path.join(__dirname, "../attachments")));
+
+
+
+app.use((req, res, next) => {
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+
+mongoose.connect(MONGODB_URI, { 
+
+})
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
